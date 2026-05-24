81693bafb258196d2db573f35685608f9313aeed
Arturo <YOUR_GITHUB_NOREPLY_EMAIL>
2026-05-24 12:30:23 -0700

    Quartz sync: May 24, 2026, 12:30 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Terror Humano",
+    pageTitle: "Borradores Para Maquinas y Pájaros",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
