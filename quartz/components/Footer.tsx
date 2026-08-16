192f90bd0627adc64f0828b8c25c1fb7e13079b1
Arturo Cordova <arturit659@gmail.com>
2026-08-16 16:40:21 -0700

    Quartz sync: Aug 16, 2026, 4:40 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -17,10 +17,22 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
          <p>
           I would love to read your comments, 
-          <a href="mailto:arturit659@gmail.com?subject=Website%20feedback">
+          <a href="mailto:arturo.borradores@gmail.com?subject=Website%20feedback">
             {" email me!"}            
           </a>
 
+          </p>
+          <p>
+            You can also 
+            <a href = "mailto:arturo.borradores@gmail.com?subject=Blog%20Subscription?content=I%20want%20to$20recieve%20updates!">
+              {" subscribe "}
+            </a>
+            to new post notifications, or
+          
+            <a href = "mailto:arturo.borradores@gmail.com?subject=Unsubscribe%20me!">
+              {" unsubscribe "}
+            </a>
+            at any time!
           </p>
       
          <ul>
